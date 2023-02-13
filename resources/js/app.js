d0d8d06153222ecedac21c84fd1cc5f664cd8655
Victor Masseron--Lucianaz <victor.masseron--lucianaz@ig2i.centralelille.fr>
2023-02-13 17:33:38 +0100

    Add breeze

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,28 +1,7 @@
 import './bootstrap';
-import { initUtils } from './utils';
-import { initAssets, selectionStart, selectionEnd} from './assets';
-import { logger } from './logger'
 
-import '../css/utils.css'
-import '../css/assets.css'
+import Alpine from 'alpinejs';
 
-import.meta.glob([
-    '../assets/**'
-])
+window.Alpine = Alpine;
 
-function init(){
-    logger("Init...")
-    initUtils();
-    initAssets();
-}
-
- if (import.meta.env.DEV) {
-    document.fonts.load('1em Roboto').then(function (a) {
-        setTimeout(() => {
-            init();
-        }, 500);
-	});
-} else {
-    window.addEventListener('load', init);
-}
-
+Alpine.start();
