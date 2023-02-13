c19b3c08d18e8147ab08051af698d4b9ca054f6a
Jeanne <jdelcourtlonguepe@gmail.com>
2023-02-13 11:29:45 +0100

    composant carte debut

--- resources/views/components/index/card.blade.php
+++ resources/views/components/index/card.blade.php
@@ -1,4 +1,35 @@
+@props(['img', 'designation','localisation', 'couchages', 'description','etoiles', 'id','lien'])
+
 <div>
     <!-- It is quality rather than quantity that matters. - Lucius Annaeus Seneca -->
-    <h1>Mettez la carte ici :)</h1>
+    <img src='{{ $img}}' alt='logement'height='120' width='200'/>
+     <div>
+        <div>
+            <h3>
+                {{ $designation }}
+            </h3>
+            <p>
+                {{ $localisation}}
+            <p>
+            <p>
+                {{ $couchages }}
+            </p>
+
+        </div>
+        <div>
+            <img src='' alt='etoile'/>
+            <p>
+                {{ $etoiles }}
+            </p>
+        </div>
+
+    </div>
+    <div>
+        <p>
+            {{ $description }}
+        </p>
+        <a href="{{ $lien }}">
+            Voir plus de détails
+        </a>
+    </div>
 </div>
