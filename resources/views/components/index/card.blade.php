e0ad4c163c0ee28ae69148d9ba8f6a26c26a8b88
HmBloqued <timothee.laffra@gmaiL.com>
2023-02-13 13:05:54 +0100

    worked on cards style and added few cards as an example

--- resources/views/components/index/card.blade.php
+++ resources/views/components/index/card.blade.php
@@ -1,40 +1,43 @@
-@props(['img', 'designation','localisation', 'couchages', 'description','etoiles', 'id','lien'])
+@props(['img', 'designation', 'localisation', 'couchages', 'description', 'etoiles', 'id', 'lien'])
 
-<div>
-    <!-- It is quality rather than quantity that matters. - Lucius Annaeus Seneca -->
-    <img src='{{ $img}}' alt='logement'height='120' width='200'/>
-     <div>
-        <div>
-            <h3>
-                {{ $designation }}
-            </h3>
+<div class="productCard">
+    <img src='{{ $img }}' alt='logement'height='120' width='200' />
+    <div class="productCardBottom">
+        <div class="productCardHeader">
+            <div class="productCardTitle">
+                <h3>
+                    {{ $designation }}
+                </h3>
+                <p>
+                    {{ $localisation }}
+                <p>
+                <div class="productCardBeds">
+                    <img src="{{ Vite::asset('resources/assets/icons/beds.svg') }}" alt='bed'height='20'
+                        width='30' />
+                    {{ $couchages }}
+                    @if ($couchages > 1)
+                        couchages
+                    @else
+                        couchage
+                    @endif
+                </div>
+
+            </div>
+            <div class="productCardRanking">
+                <img src="{{ Vite::asset('resources/assets/icons/stars.svg') }}" alt='star'height='20'
+                    width='30' />
+                <label>
+                    {{ $etoiles }}
+                </label>
+            </div>
+        </div>
+        <div class="productCardFooter">
             <p>
-                {{ $localisation}}
-            <p>
-            <p>
-                <img src="{{Vite::asset('resources/assets/icons/beds.svg')}}" alt='bed'height='20' width='30'/>
-                {{ $couchages }}
-                @if($couchages==1)
-                    couchage
-                @else
-                    couchages
-                @endif
+                {{ $description }}
             </p>
-
+            <a href="{{ $lien }}">
+                Voir plus de détails
+            </a>
         </div>
-        <div>
-            <img src="{{Vite::asset('resources/assets/icons/stars.svg')}}" alt='star'height='20' width='30'/>
-            {{ $etoiles }}
-
-        </div>
-
-    </div>
-    <div>
-        <p>
-            {{ $description }}
-        </p>
-        <a href="{{ $lien }}">
-            Voir plus de détails
-        </a>
     </div>
 </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,13 +7,15 @@
     <title>Vincent Masseron</title>
 
     <!-- Styles -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/css/index.css', 'resources/js/app.js'])
 </head>
 <body>
     <div id="content">
         <x-header></x-header>
-        <x-form.button disabled small icon="{{ Vite::asset('resources/assets/icons/download.svg') }}">Bouton</x-form.button>
-        <x-index.card img="{{Vite::asset('resources/assets/logo/long-name.svg')}}" designation="Produit 1" localisation="Bordeaux" couchages="5" description="dfghjkl" etoiles="5" id="6" lien="fgkiol" ></x-index.card>
+        <x-index.card img="{{Vite::asset('resources/assets/demo/demo1.jpg')}}" designation="Produit 1" localisation="Donville-les-Bains, Normandie, France" couchages="5" description="On remarque une grande similitude entre les courbes générées avant et après la modification. D’après nos recherches, la principale différence entre les 2 programmes maintenant concerne la mémoire." etoiles="5,0" id="6" lien="lorem" ></x-index.card>
+        <x-index.card img="{{Vite::asset('resources/assets/demo/demo1.jpg')}}" designation="Produit 1" localisation="Bordeaux" couchages="5" description="Rien à dire" etoiles="5,0" id="6" lien="fgkiol" ></x-index.card>
+        <x-index.card img="{{Vite::asset('resources/assets/demo/demo1.jpg')}}" designation="Produit au nom beaucoup trop long" localisation="Bordeaux" couchages="5" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, fuga doloribus optio rem molestiae in reprehenderit magnam accusamus natus soluta sequi, similique possimus vel animi dolor rerum dolore quibusdam sint?" etoiles="5,0" id="6" lien="fgkiol" ></x-index.card>
+        <x-index.card img="{{Vite::asset('resources/assets/demo/demo1.jpg')}}" designation="Produit 1" localisation="Bordeaux" couchages="5" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, fuga doloribus optio rem molestiae in reprehenderit magnam accusamus natus soluta sequi, similique possimus vel animi dolor rerum dolore quibusdam sint?" etoiles="5,0" id="6" lien="fgkiol" ></x-index.card>
     </div>
 </body>
 </html>
