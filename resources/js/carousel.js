7dd311fc4287338939256a7aa7fb1468c57f3432
HmBloqued <timothee.laffra@gmaiL.com>
2023-02-14 12:58:51 +0100

    fully working carousel without animation

--- resources/js/carousel.js
+++ resources/js/carousel.js
@@ -2,21 +2,24 @@ import { logger } from './logger'
 import carouselPrevious from '/resources/assets/icons/carousel/previous.svg'
 import carouselNext from '/resources/assets/icons/carousel/next.svg'
 
-let carouselSources = {
-    "src": ["https://images.unsplash.com/photo-1674592309639-39067f6a8111?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1776&q=80",
-        "https://images.unsplash.com/photo-1675410200389-903e50c46cbf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
-        "https://images.unsplash.com/photo-1509817177816-ca503fa03f60?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
-        "https://images.unsplash.com/photo-1675208986290-a72414630bbe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
-        "https://plus.unsplash.com/premium_photo-1671643642938-ea7b23ad4d8f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
-        "https://images.unsplash.com/photo-1675191475848-5dc8ec4bfafa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2128&q=80",
-        "https://images.unsplash.com/photo-1675104180341-4b075869b3bf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2232&q=80"]
-}
+let carouselSources;
 let carouselImages = [];
-
-console.log(carouselSources);
 
 export function initCarousel() {
     logger("Initializing carousel.js...");
+
+    // TEMP
+
+    carouselSources = {
+        "src": ["https://images.unsplash.com/photo-1674592309639-39067f6a8111?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1776&q=80",
+            "https://images.unsplash.com/photo-1675410200389-903e50c46cbf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+            "https://images.unsplash.com/photo-1509817177816-ca503fa03f60?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+            "https://images.unsplash.com/photo-1675208986290-a72414630bbe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+            "https://plus.unsplash.com/premium_photo-1671643642938-ea7b23ad4d8f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+            "https://images.unsplash.com/photo-1675191475848-5dc8ec4bfafa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2128&q=80",
+            "https://images.unsplash.com/photo-1675104180341-4b075869b3bf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2232&q=80"]
+    }
+    carouselImages = carouselSources.src;
     generateCarousel(true);
 
     var popup = document.getElementById("imagePopup");
@@ -37,16 +40,20 @@ export function initCarousel() {
                 if (popup != undefined)
                     popup.style.display = "flex";
             }
+            else if (e.target.classList.contains("carouselQueueItem")) {
+                generateCarousel(false, e.target.src);
+            }
         });
 }
 
 
 /**
- *
+ * @brief
  * @param {boolean} next -> false to display the previous image, true to display the next one
+ * @param {String} src   -> To display a certain image
  * @returns
  */
-function generateCarousel(next) {
+function generateCarousel(next, src) {
     var carousel = document.getElementById("carousel");
     if (carousel == undefined) {
         logger("Carousel manquant", "error");
@@ -59,19 +66,22 @@ function generateCarousel(next) {
         logger("Popup manquant", "error");
         return;
     }
-    if (carouselImages.length == 0)
-        carouselImages = carouselSources.src;
-
-    console.log(carouselImages);
-    console.log(carouselImages.length);
 
     // Création :
     var content = "<div class='carouselLeft'>";
     content += "<img class = 'carouselNext' src='" + carouselNext + "'>";
     content += "<img class = 'carouselPrevious' src='" + carouselPrevious + "'>";
-
-
-    if (next)
+    console.log(src);
+    if(src != undefined){
+        for (let i = 0; i < carouselImages.length; i++) {
+            if(src == carouselImages[i]){
+                for (let y = 0; y < i; y++) {
+                    carouselImages.push(carouselImages.shift());
+                }
+            }
+        }
+    }
+    else if (next)
         carouselImages.push(carouselImages.shift());
     else
         carouselImages.unshift(carouselImages.pop());
@@ -82,21 +92,9 @@ function generateCarousel(next) {
     content += "<img class='carouselImageDisplayed' src='" + carouselImages[1] + "'/>";
     content += "</div><div class='carouselRight'>";
     for (let i = 1; i < carouselImages.length; i++) {
-        content += "<img src='" + carouselImages[i] + "'/>";
+        content += "<img class='carouselQueueItem' src='" + carouselImages[i] + "'/>";
     }
 
-    // if (next) {
-    //     for (let i = 2; i < sources.length; i++) {
-    //         content += "<img src='" + sources[i] + "'/>";
-    //     }
-    //     content += "<img src='" + sources[0] + "'/>";
-    // }
-    // else {
-    //     // content += "<img src='" + sources[sources.length - 1] + "'/>";
-    //     for (let i = 0; i < sources.length - 1; i++) {
-    //         content += "<img src='" + sources[i] + "'/>";
-    //     }
-    // }
     content += "</div>";
     document.getElementById("carousel").innerHTML = content;
 
