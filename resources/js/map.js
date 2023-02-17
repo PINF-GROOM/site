be0ecade19f89d92d3531fb368ebc85baed72211
HmBloqued <timothee.laffra@gmaiL.com>
2023-02-17 11:22:15 +0100

    fixed errors with calendar.js (new asset.js) Close #11

--- resources/js/map.js
+++ resources/js/map.js
@@ -6,12 +6,14 @@ var lon = 2.349903;
 var macarte = null;
 
 export function initMap() {
-    logger("initMap" , "info");
-    macarte = L.map('map').setView([lat, lon], 11);
-    // Link to data source
-    L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
-        attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a> - rendu <a href="//openstreetmap.fr">OSM France</a>',
-        minZoom: 1,
-        maxZoom: 20
-    }).addTo(macarte);
+    if (document.getElementById("map") != undefined) {
+        logger("initMap", "info");
+        macarte = L.map('map').setView([lat, lon], 11);
+        // Link to data source
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+            attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a> - rendu <a href="//openstreetmap.fr">OSM France</a>',
+            minZoom: 1,
+            maxZoom: 20
+        }).addTo(macarte);
+    }
 }
